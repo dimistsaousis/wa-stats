6621426224aadebbd4fa2d10ed06a67f22299d4c
Dimis Tsaousis <dimis.tsaousis@oaknorth.ai>
2020-04-21 00:55:35 +0100

    features

--- features.py
+++ features.py
@@ -1,5 +1,3 @@
-from message_parser import Message
-from functools import wraps
 import re
 
 
@@ -8,17 +6,60 @@ def feature(fn):
     return fn
 
 
-@feature
-def laugh_count(msg: Message):
-    if '🤣' in msg.message or '😂' in msg.message:
+def message_feature(fn):
+    "Will map the function to the message column"
+
+    def wrapper(df):
+        return df.message.map(fn)
+
+    wrapper.__name__ = '__feature__' + fn.__name__
+    return wrapper
+
+
+def row_feature(fn):
+    "Will apply the function to each row"
+
+    def wrapper(df):
+        return df.apply(fn, axis=1)
+
+    wrapper.__name__ = '__feature__' + fn.__name__
+    return wrapper
+
+
+def get_date_features(df):
+    df['year'] = df.timestamp.dt.year
+    df['quarter'] = df.timestamp.dt.quarter
+    df['month'] = df.timestamp.dt.month
+    df['month_name'] = df.timestamp.dt.month_name()
+    df['date'] = df.timestamp.dt.date
+    df['day'] = df.timestamp.dt.day
+    df['day_name'] = df.timestamp.dt.day_name()
+    df['dayofweek'] = df.timestamp.dt.dayofweek
+    df['dayofyear'] = df.timestamp.dt.dayofyear
+    df['hour'] = df.timestamp.dt.hour
+    df['minute'] = df.timestamp.dt.minute
+    df['second'] = df.timestamp.dt.second
+    df['date'] = df.timestamp.dt.date
+    df['time'] = df.timestamp.dt.time
+    return df
+
+
+@message_feature
+def laugh_count(msg):
+    if '🤣' in msg or '😂' in msg:
         return 1
-    elif re.match(r'\b(?:a*(?:ha)+h?|(?:l+o+)+l+)\b', msg.message, re.I):
+    elif re.match(r'\b(?:a*(?:ha)+h?|(?:l+o+)+l+)\b', msg, re.I):
         return 1
     return 0
 
 
+@row_feature
+def message_count(row):
+    return 1 if not row.auto else 0
+
+
 @feature
-def username(msg: Message):
+def username(df):
     USERS = {
         'You': 'Dimis T.',
         'La Révolution Capitale': None,
@@ -35,46 +76,51 @@ def username(msg: Message):
         'Nikolaou': 'Nikos N.',
         'Skasis': 'Antonis S.',
     }
-    return USERS.get(msg.user, msg.user)
+    return df.user.map(USERS.get)
 
 
-@feature
-def picture_count(msg: Message):
-    if 'picture omitted' in msg and msg.auto:
+def media_counts(media_name, row):
+    if media_name + ' omitted' in row.message and row.auto:
         return 1
     return 0
 
 
-@feature
-def video_count(msg: Message):
-    if 'video omitted' in msg and msg.auto:
+@row_feature
+def image_count(row):
+    return media_counts('image', row)
+
+
+@row_feature
+def video_count(row):
+    return media_counts('video', row)
+
+
+# @row_feature
+# def audio_count(row):
+#     return media_counts('audio', row)
+
+
+# @row_feature
+# def document_count(row):
+#     return media_counts('document', row)
+
+
+# @row_feature
+# def gif_count(row):
+#     return media_counts('GIF', row)
+
+
+# @row_feature
+# def contact_card_count(row):
+#     return media_counts('Contact card', row)
+
+
+@message_feature
+def question_count(msg):
+    if msg[-1] == '?':
         return 1
     return 0
 
 
-@feature
-def audio_count(msg: Message):
-    if 'audio omitted' in msg and msg.auto:
-        return 1
-    return 0
-
-
-@feature
-def document_count(msg: Message):
-    if 'document omitted' in msg and msg.auto:
-        return 1
-    return 0
-
-
-@feature
-def gif_count(msg: Message):
-    if 'GIF omitted' in msg and msg.auto:
-        return 1
-    return 0
-
-
-@feature
-def contact_card_count(msg: Message):
-    if 'Contact card omitted' in msg and msg.auto:
-        return 1
-    return 0
+def message_size(msg):
+    return len(msg)
